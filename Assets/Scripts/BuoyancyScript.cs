e94dc1a6bcdaa19282576aea1fd1d0dca86176c1
SjoerdJakobs <18284@ma-web.nl>
2017-01-24 04:08:15 +0100

    fixed repo
    fixed repo

---
+++ Assets/Scripts/BuoyancyScript.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BuoyancyScript : MonoBehaviour {
+
+    [SerializeField]
+    private LayerMask waterMask;
+
+    [SerializeField]
+    private List<Transform> floatPoints;
+
+    private Rigidbody rigid;
+
+    private Vector3 center;
+    private Vector3 lastCenter;
+    private Vector3 waterLine;
+    //private Vector3 center;
+
+    [SerializeField]
+    private bool singleBuoyancyPoints;
+    private bool inWater;
+
+    [SerializeField]
+    private float waterDensity = 25;
+    [SerializeField]
+    private float shipDepth;
+    [SerializeField]
+    private float waterDragg;
+    [SerializeField]
+    private float stabeliser = 2;
+    private float velocity;
+    private float upwardForce;
+
+    private int Counter;
+   
+    private Dictionary<Transform, Timer> timerMap;
+    void Awake()
+    {
+        timerMap = new Dictionary<Transform, Timer>();
+
+        foreach (Transform T in floatPoints)
+        {
+            timerMap.Add(T, T.gameObject.AddComponent<Timer>());
+        }
+    }
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        /*GameObject[] getFloatPoints = GetComponentsInChildren<GameObject>();
+        foreach(GameObject G in getFloatPoints)
+        {
+            if(G.CompareTag("Floatpoints"))
+            {
+                floatPoints.Add(G.transform);
+            }
+        }*/
+        //floatPoints.Add(GameObject.FindGameObjectsWithTag("FloatPoint"));
+    }
+	
+	void FixedUpdate () {
+        velocity = Vector3.Magnitude(rigid.velocity);
+        CheckBuoyancyPoints();
+        CheckInWater();
+        CheckWaterline();
+        FloatAndDragg();
+    }
+
+    void CheckWaterline()
+    {
+        if (singleBuoyancyPoints)
+        {
+            Ray rayDown = new Ray(center, Vector3.down);
+            Ray rayUp = new Ray(center, Vector3.up);
+            RaycastHit hit;
+            if (Physics.Raycast(rayDown, out hit, 5, waterMask, QueryTriggerInteraction.Collide) || Physics.Raycast(rayUp, out hit, 25, waterMask, QueryTriggerInteraction.Collide))
+            {
+                waterLine = hit.point - new Vector3(0,shipDepth,0);
+                //print(waterLine + "waterline");
+            }
+        }
+    }
+
+    void CheckBuoyancyPoints()
+    {
+        center = Vector3.zero;
+        Counter = 0;
+        foreach(Transform T in floatPoints)
+        {
+            Ray rayDown = new Ray(T.position, Vector3.down);
+            Ray rayUp = new Ray(T.position, Vector3.up);
+            RaycastHit hit;
+            if (Physics.Raycast(rayDown, out hit, 5, waterMask, QueryTriggerInteraction.Collide) || Physics.Raycast(rayUp, out hit, 25, waterMask, QueryTriggerInteraction.Collide))
+            {
+                if(T.position.y <= hit.point.y - shipDepth)
+                {
+                    if (!timerMap[T].IsRunning)
+                    {
+                        timerMap[T].StartTimer();
+                    }
+                    if (singleBuoyancyPoints)
+                    {
+                        //print(timerMap[T].TimerTime);
+                        if (timerMap[T].TimerTime < stabeliser)
+                        {
+                            rigid.AddForceAtPosition(new Vector3(0, ((timerMap[T].TimerTime / stabeliser) * waterDensity)/floatPoints.Count, 0), T.position);
+                            //print(timerMap[T].TimerTime / stabeliser + "keer" );
+                            //print(timerMap[T].TimerTime);
+                        }
+                        else
+                        {
+                            rigid.AddForceAtPosition(new Vector3(0, waterDensity/floatPoints.Count, 0), T.position);
+                        }
+                    }
+                    center += T.position;
+                    Counter++;
+                }
+                else
+                {
+                    if (timerMap[T].IsRunning)
+                    {
+                        timerMap[T].StopTimer();
+                    }
+                }
+            }
+        }
+        center = center / Counter;
+        if(Vector3.Distance(center,lastCenter) > 0.5f)
+        {
+            //center = (center + lastCenter + lastCenter + lastCenter) / 4;
+        }
+        lastCenter = center;
+    }
+
+    void CheckInWater()
+    {
+        if (center.y < waterLine.y)
+        {
+            upwardForce = (1 - Mathf.Clamp(waterLine.y / transform.position.y,0.1f,1)/ (Mathf.Clamp(waterLine.y / transform.position.y, 0.1f, 1)+1f)) * waterDensity;
+            inWater = true;
+            //print(upwardForce+ "force");
+        }
+        else
+        {
+            inWater = false;
+        }
+    }
+
+    void FloatAndDragg()
+    {
+        if (inWater)
+        {
+            if (Counter != 0 && !singleBuoyancyPoints)
+            {
+                rigid.AddForceAtPosition(new Vector3(0, upwardForce, 0), center);
+            }
+            if (velocity > 0.8f)
+            {
+                rigid.AddForce(rigid.velocity * -1 * waterDragg);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StatsSystem/LivingEntity.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivingEntity : MonoBehaviour , IAmLiving
+{
+    [SerializeField]
+    protected Stats userStats;
+    protected int powerPoints;
+    protected IAmLiving enemyCaster;
+    
+    protected bool dead;
+    protected bool isLVLing = false;
+
+    public event System.Action OnDeath;
+
+
+    // Use this for initialization
+    protected virtual void Start () {
+	
+	}
+
+    #region UPDATE
+    protected virtual void Update()
+    {
+
+    }
+    #endregion
+
+    #region INTERFACE
+    public void TakeDamage()
+    {
+
+    }
+    #endregion
+}
+
+#region USERSTATS
+
+[System.Serializable]
+public class Stats
+{
+    private float Level = 1;
+    public float xp = 0;
+    public float xpOnDeath;
+    public float maxHealth = 100;
+    public float health;
+    public float healthRegen = 1;
+    public float maxMana = 100;
+    public float mana;
+    public float manaRegen = 8;
+    public float armor = 20;
+    public float armorPen = 0;
+    public float magicResist = 15;
+    public float magicPen = 0;
+    public float moveMentspeed = 10;
+    public float sizeMod = 1;
+    public float attackDamage = 10;
+    public float magicDamage = 0;
+    public float attackspeed = 1.2f;
+    public float cooldownReduction = 0;
+    public int powerPointsPerLVL = 10;
+}
+
+#endregion
